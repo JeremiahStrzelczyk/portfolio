cc100e490a04dc74111f5fffa07ad6eb67d5993d
JeremiahStrzelczyk <Jeremiah.Strzelczyk@WSU.edu>
2023-03-05 15:23:16 -0800

    adding assets

---
+++ app/client/src/pages/ProjectData.js
@@ -0,0 +1,66 @@
+const SpeedScore = {
+  styles: {
+    backgroundColor: "#0d6efd",
+  },
+  title: "SpeedScore",
+  abstract:
+    "Full-stack web application for speed golfers to record stats, manage friends, and access a feed mode",
+  point1:
+    "Implemented Google authentication for user login and used accessibility standards to ensure compliance and improve app usability",
+  point2:
+    "Designed and developed a badge system that depends on golfer's stats to incentivize engagement and improve user retention",
+  point3:
+    "Collaborated with a team of developers to ensure code quality, maintainability, and scalability",
+  summary:
+    "Overall, SpeedScore is a showcase of my technical skills in full-stack web development and my ability to collaborate with a team towards a common goal.",
+  //   stack: "React JavaScript HTML CSS Express.js Node.js MongoDB",
+  stack: [
+    "JavaScript,",
+    "HTML,",
+    "CSS,",
+    "Express.js,",
+    "Node.js,",
+    "MongoDB",
+    "React",
+    "Passport.js",
+  ],
+  image: {
+    alt: "SpeedScore",
+    width: "1186",
+    height: "848",
+  },
+
+  //   gif: { speedscore },
+  //   alt: "SpeedScore",
+};
+
+const Featherdusters = {
+  styles: {
+    backgroundColor: "#85756a",
+  },
+  title: "Featherdusters LLC",
+  abstract:
+    "Multi-page, mobile-first website built, maintained, and refactored for a local business",
+
+  point1:
+    "Led front-end development and collaborated with business owners to gather requirements and create a website that met their needs",
+
+  point2:
+    "Implemented accessibility standards to ensure compliance and improve app usability for a wider range of users",
+
+  point3:
+    "Improved website performance through testing, debugging, and optimization",
+
+  summary:
+    "Overall, this project showcases my skills in front-end development and collaboration, and highlights my ability to create effective digital solutions for businesses.",
+
+  //   stack: "React  JavaScript  HTML  CSS",
+  stack: ["JavaScript,", "HTML,", "CSS", "React,"],
+  image: {
+    alt: "Featherdusters LLC",
+    width: "1209",
+    height: "851",
+  },
+};
+
+export { SpeedScore, Featherdusters };
