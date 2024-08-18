459f9115687bbcc2666211ff66fb6c9ac0985824
JeremiahStrzelczyk <78121398+JeremiahStrzelczyk@users.noreply.github.com>
2024-08-17 22:19:48 -0700

    Updated service worker to remove redundant cache

---
+++ app/client/Resources.md
@@ -0,0 +1,8 @@
+Arizona Departmet of Education. (n.d.). azed. *Arizona’s Language Development Approach*. https://www.azed.gov/sites/default/files/2020/01/Arizona%27s%20Language%20Development%20Approach_December%20State%20Board%20Meeting_2019_rev2-25.pdf?id=5e149c9e03e2b32094fa7b5c
+Arizona Department of Education. (2019). azed. *AZELLA accommodations manual*. https://www.azed.gov/sites/default/files/2023/10/2023%20AZELLA%20Accommodations%20Manual.pdf
+Arizona Department of Education. (2024). azed. *SEI Model Implementation Guide 2023-2024*. https://www.azed.gov/sites/default/files/2023/05/SEI_ModelImplementationGuide2023-2024FINAL.pdf
+Arizona State Legislature. (2024). azleg. https://www.azleg.gov/viewdocument/?docName=https://www.azleg.gov/ars/15/00751.htm
+Colorin Colorado. (1993). *Program models for teaching english language learners*. https://www.colorincolorado.org/article/program-models-teaching-english-language-learners
+Office of English Language Acquisition Services. (n.d.). azed. *Arizona english learner determination and services*. https://www.azed.gov/sites/default/files/2023/05/EL_DeterminationAndServices-EL_ProgramsFlow-Chart_Updated2023.pdf
+
+
--- app/client/public/service-worker.js
+++ app/client/public/service-worker.js
@@ -1,9 +1,39 @@
-const CACHE_NAME = "jeremiah-strzelczyk-portfolio-v1";
+// const CACHE_NAME = "jeremiah-strzelczyk-portfolio-v1";
+// const urlsToCache = [
+//   "/",
+//   // "/index.html",
+//   "/manifest.json",
+//   // add any other assets you want to cache here
+// ];
+
+// this.addEventListener("install", (event) => {
+//   // Perform install steps
+//   event.waitUntil(
+//     caches.open(CACHE_NAME).then((cache) => {
+//       console.log("Opened cache");
+//       return cache.addAll(urlsToCache);
+//     })
+//   );
+// });
+
+// this.addEventListener("fetch", (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then((response) => {
+//       // Cache hit - return response
+//       if (response) {
+//         return response;
+//       }
+//       return fetch(event.request);
+//     })
+//   );
+// });
+const CACHE_NAME = "jeremiah-strzelczyk-portfolio-v2"; // Updated version
 const urlsToCache = [
   "/",
-  // "/index.html",
   "/manifest.json",
-  // add any other assets you want to cache here
+  "/main.84e69138.js", // Add your JS file here
+  "/main.a321c9c5.css", // Add your CSS file here
+  // Add other assets you want to cache
 ];
 
 this.addEventListener("install", (event) => {
@@ -16,6 +46,23 @@ this.addEventListener("install", (event) => {
   );
 });
 
+this.addEventListener("activate", (event) => {
+  // Remove old caches
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            console.log("Deleting old cache:", cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
 this.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
@@ -23,7 +70,24 @@ this.addEventListener("fetch", (event) => {
       if (response) {
         return response;
       }
-      return fetch(event.request);
+      return fetch(event.request).then((response) => {
+        // Check if we received a valid response
+        if (!response || response.status !== 200 || response.type !== "basic") {
+          return response;
+        }
+
+        // IMPORTANT: Clone the response. A response is a stream
+        // and because we want the browser to consume the response
+        // as well as the cache consuming the response, we need
+        // to clone it so we have two streams.
+        const responseToCache = response.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+      });
     })
   );
 });
