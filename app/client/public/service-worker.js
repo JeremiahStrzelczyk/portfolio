b24fd96202d0793946c8ef8e2fd2ab1a41d2fe2f
JeremiahStrzelczyk <78121398+JeremiahStrzelczyk@users.noreply.github.com>
2024-08-18 12:12:18 -0700

    Update service-worker.js

--- app/client/public/service-worker.js
+++ app/client/public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "jeremiah-strzelczyk-portfolio-v2"; // Increment this version with each deployment
+const CACHE_NAME = "jeremiah-strzelczyk-portfolio-v2.1"; // Increment this version with each deployment
 const urlsToCache = [
   "/",
   "/manifest.json",
